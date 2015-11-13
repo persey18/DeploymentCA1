85d4dc5164817b5b047453ce7a637865f30fa6b7
NCI <nci@nci-VirtualBox.(none)>
2015-11-13 19:30:08 +0000

    add create update destroy ability for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,47 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
   def index 
-    
-  end
+    @posts = Post.all.order("created_at DESC")
+   end
+   
+   def show 
+   
+   end
+   
+   def new 
+     @post = Post.new
+   end
+   
+   def create 
+     @post = Post.new(post_params)
+     if @post.save
+      redirect_to @post #redirect to post if it can be saved 
+    else
+      render 'new'
+   end
+  end 
+   def edit 
+   end
+   
+   def update
+     if @post.update(post_params) 
+       redirect_to @post
+     else
+       render 'edit'
+     end
+   end
+   
+   def destroy
+     @post.destroy
+     redirect_to root_path
+   end
+   
+   def find_post
+     @post = Post.find(params[:id])
+     #grab post
+   end
+   
+   def post_params
+   params.require(:post).permit(:title, :contact, :description) #define what the post params are 
+   end
 end
